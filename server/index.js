663b693629eefa22855134f4dfb75bed1423aac1
Daniel Escobar <escobard90@gmail.com>
2018-01-29 18:25:27 -0700

    doc: updated documentation on basics of express

--- server/index.js
+++ server/index.js
@@ -1,17 +1,47 @@
 
-// need to use ES5 syntax for now to use express
-// more about express here: https://expressjs.com/en/starter/hello-world.html
+// for detailed notes on express / passport authentication 
+// mongodb user retention / token creation go to 
+// https://github.com/escobard/Advanced-React-and-Redux/tree/master/auth
+
+// official express here: https://expressjs.com/en/starter/hello-world.html
 const express = require('express');
 
 // this invokes the express helpers, which has prototypes that allow us to 
 // better manipulate node runtime HTTP requests / responses
+
+// express app to register this route handler with
 const app = express();
 
-// receives response on main route
+// watches for incoming 'get' HTTP request method within the '/' route
+// this is called a route handler - more here: https://expressjs.com/en/starter/basic-routing.html
+
+/* 
+
+other HTTP requests supported include:
+
+app.get - GET HTTP request - used to retreive or fetch info - most common type of request
+
+app.post - POST HTTP request - used to send info
+
+app.put - PUT HTTP request - used to update data
+
+app.delete - DELETE HTTP request - used to delete data
+
+app.patch - update one or two properties of something 
+
+*/
+
 app.get('/',
 
 // handles the request response as req and respose as res
+
+// req is the object representing the incoming request
+// res is the object representing the outgoing response
 (req, res) => {
+
+	// displays important request data
+	console.log('headers: ', req.headers)
+	console.log('raw headers: ', req.rawHeaders)
 
 	// sends the json object below to the route specified
 	res.send({ hi: 'there' });
