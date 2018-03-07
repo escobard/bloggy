ab4a25e3928036b8a405b52aa2e6a59d7a443e7b
Daniel Escobar <escobard90@gmail.com>
2018-03-06 19:17:04 -0700

    feat: updated reducer store callback, updated primary colors

--- client/src/index.js
+++ client/src/index.js
@@ -4,24 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom'
 
-import {indigo500} from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import App from './components/app';
 import reducers from './reducers';
-const muiTheme = getMuiTheme({
-  palette: {
-    backgroundColor: indigo500,
-  },
-});
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(reducers, {}, applyMiddleware())
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
-	  <MuiThemeProvider muiTheme={muiTheme}>
+	  <MuiThemeProvider>
 	    <App />
 	  </MuiThemeProvider>
 	</BrowserRouter>
