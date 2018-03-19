12a69d4dbac42aba1488f52084417a3b5e449e2a
Daniel Escobar <escobard90@gmail.com>
2018-03-19 16:25:52 -0600

    chore: added footer jest snapshot - will need to set up constants / proptypes for init state

--- client/test/components/header.test.js
+++ client/test/components/header.test.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'
 import Header from '../../src/components/Header';
-import renderer from 'react-test-renderer';
 import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-const muiTheme = getMuiTheme();
-describe('Header - with mock state', () => {
+describe('Header snapshot', () => {
 	const initialState = {output:100}
 	const mockStore = configureStore()
 	let store,container
