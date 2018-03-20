87a588075ed777b14e18c068c764c207cb08527f
Daniel Escobar <escobard90@gmail.com>
2018-03-20 16:15:40 -0600

    chore: added base test for async actions, need to finalize still not working

--- client/test/jest_setup.js
+++ client/test/jest_setup.js
@@ -1,5 +1,6 @@
-import Enzyme, { shallow, render, mount, expect } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow, render, mount, expect } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import "babel-polyfill";
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 
