baf7053cf5ec0faa1fbfd51d99cb24b08f4459c5
Daniel Escobar <escobard90@gmail.com>
2018-03-26 16:34:30 -0600

    feat: created stripe charge within the stipe post route API handler

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -12,10 +12,30 @@ const stripe = require("stripe")(stripeSecret);
 
 module.exports = function(app) {
 
-	// since we are using the bodyParser library, we can call json objects for the 
-	// POST request with req.body
+	// since we are using the bodyParser library, we can call parse objects for the 
+	// POST request into req.body - this isn't a default function of express
 	app.post(stripeRoute, (req, res) => {
+
+		// this should return the stripe API payment token, which contains all the necessary client data
 		console.log(req.body)
+
+		/* more on how charges with stripe work here: https://stripe.com/docs/api/node#create_charge 
+		*/
+		stripe.charges.create({
+			// amount - the amount of money in cents that the transaction
+			// is expected to charge the user in CENTS
+
+			// declared again from the front end to double check the amount between the client / server sides
+			// before sending off to the stripe authentication API
+			amount: 500,
+
+			// default currency is again USD, but can be set to pretty much anything else 
+			currency: 'usd',
+			description: '$5 for 5 credits',
+
+			// this is the TOKEN that we received from the client side request to the stripe API
+			source: req.body.id
+		})
 	});
 };
 
