cf4290f92eb7c05f2023e8cc5087fef9f73db7ba
Daniel Escobar <escobard90@gmail.com>
2018-03-27 17:21:57 -0600

    fix: fixed route handler

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,6 @@ const authRoutes = require("./authRoutes");
 const billingRoutes = require("./billingRoutes");
 
 module.exports = function(app) {
-	
-	app.get("/", (req, res) => {
-		console.log("headers: ", req.headers);
-		// sends the json object below to the route specified
-		res.send({ hi: "there" });
-	});
 
 	authRoutes(app);
 	billingRoutes(app);
