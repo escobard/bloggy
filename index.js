695ad886d3cdcb4e0abac02959a8d575ad5517f8
Daniel Escobar <escobard90@gmail.com>
2018-03-27 17:27:05 -0600

    fix: fixed wrong route handlers

--- index.js
+++ index.js
@@ -34,7 +34,18 @@ app.use(passport.session());
 	routes(app)
 */
 require("./routes")(app);
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
 
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 let server = app.listen(port, () => {
 	console.log("server listening at port %s", port);
 	console.log("server listening on env %s", process.env.NODE_ENV);
