0d401134684c10e6f169528b662b2a4d2370a1fb
Daniel Escobar <escobard90@gmail.com>
2018-04-03 15:11:12 -0600

    feat: added middleware to check if the user has greater than 1 credits

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,7 +1,12 @@
 const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
 
 module.exports = app => {
-	app.post('/api/jobs', requireLogin, (req, res) =>{
+
+	// we can add as many middlewares as we want to a route handler
+	// the only gotcha is that the middlewares MUST be added in the order 
+	// we want the middlewares to run
+	app.post('/api/jobs', requireLogin, requireCredits, (req, res) =>{
 
 	})
 }
