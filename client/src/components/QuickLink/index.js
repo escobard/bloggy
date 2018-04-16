b79b146533725d47412f565479e51c9328d2df01
Daniel Escobar <escobard90@gmail.com>
2018-04-16 14:47:25 -0600

    style: added base styles to dashboard CTA icons

--- client/src/components/QuickLink/index.js
+++ client/src/components/QuickLink/index.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {
-	RaisedButton
-} from "material-ui"
-import { Link } from "react-router-dom"
+import { RaisedButton, Card } from "material-ui";
+import { Link } from "react-router-dom";
+import DashIcon from "material-ui/svg-icons/action/dashboard";
 
-import styles from "./styles.scss"
+import styles from "./styles.scss";
 
 // add images and finish layout of 2x2 for each cta
-export default function QuickLink(routeObject){
-	let {img, route} = routeObject.route
+export default function QuickLink(routeObject) {
+	let { img, route } = routeObject.route;
 	return (
 		<div className="dashboard-cta col-md-3">
 			<Link to={`/dashboard/${route}`}>
-		    	<RaisedButton className="">
-		    		{route}
-		    	</RaisedButton>
-	    	</Link>
-    	</div>
-	)
+				<Card className="card">
+					<img src="" alt={`${route} icon`} />
+					<h4>{route}</h4>
+				</Card>
+			</Link>
+		</div>
+	);
 }
 
 QuickLink.defaultProps = {
-  routeObject: {
-  	img: '',
-  	route: "test"
-  }
+	routeObject: {
+		img: "",
+		route: "test"
+	}
 };
 
 QuickLink.propTypes = {
-  routeObject: PropTypes.object
+	routeObject: PropTypes.object
 };
