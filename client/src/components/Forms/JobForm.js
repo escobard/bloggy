8b70df8826b068d52e9a37e34f86af38d670cf74
Daniel Escobar <escobard90@gmail.com>
2018-04-16 18:12:35 -0600

    feat: added base component for the new job form, and forms folder

---
+++ client/src/components/Forms/JobForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types";
+
+export default class JobForm extends Component{
+	render(){
+		return(
+
+		)
+	}
+}
+
+JobForm.defaultProps = {
+	test: "test"
+};
+
+JobForm.propTypes = {
+	test: PropTypes.string
+};
