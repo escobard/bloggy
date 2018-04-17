e679a51c3dd76216d41548cd6e5169bcf909ad6b
Daniel Escobar <escobard90@gmail.com>
2018-04-16 18:54:02 -0600

    feat: added TextField input to redux-form - success!

--- client/src/components/Forms/JobForm.js
+++ client/src/components/Forms/JobForm.js
@@ -1,17 +1,28 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 // this is similar to connect from react-redux
-import { reduxForm } from "redux-form"
+import { reduxForm, Field } from "redux-form";
+import TextField from "material-ui/TextField";
 
 // more on this on redux-form.com
 // shows a ton of examples that we can use for validation and quick validation helpers
 // look at https://redux-form.com/7.3.0/examples/fieldlevelvalidation/ for login user creation val
-class JobForm extends Component{
-	render(){
-		return(
-
-		)
+class JobForm extends Component {
+	renderField() {
+		return <TextField hintText="Hint Text" />;
+	}
+	render() {
+		return (
+			<div>
+				<TextField
+					hintText="Ea. Full stack developer"
+					floatingLabelText="Enter a Job Title"
+				>
+					<Field type="text" name="surveyTitle" component="input" />
+				</TextField>
+			</div>
+		);
 	}
 }
 
@@ -25,5 +36,5 @@ JobForm.propTypes = {
 
 export default reduxForm({
 	// these options customize how we want the form to behave
-	form: 'jobForm'
-})(SurveyForm)
+	form: "jobForm"
+})(JobForm);
--- client/src/containers/newJob.js
+++ client/src/containers/newJob.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import { connect } from "react-redux"
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
+
+import JobForm from "../components/Forms/JobForm"
 
 class NewJob extends Component {
 	render() {
 		return (
 			<div className="jobs">
 				<h1 className="title">Add a New Job</h1>
+				<JobForm />
 			</div>
 		);
 	}
