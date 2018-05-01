c34ad39f8d6f8c2b6c6557ac25960b1d4ea46451
Daniel Escobar <escobard90@gmail.com>
2018-04-30 22:14:10 -0600

    refactor: improved scalability for form inputs by using static data

--- client/src/components/Forms/JobForm/index.js
+++ client/src/components/Forms/JobForm/index.js
@@ -8,33 +8,23 @@ import InputField from "../Fields/InputField";
 // more on this on redux-form.com
 // shows a ton of examples that we can use for validation and quick validation helpers
 // look at https://redux-form.com/7.3.0/examples/fieldlevelvalidation/ for login user creation val
+
+import { jobFormFields } from "../../../constants";
+
 class JobForm extends Component {
+	renderInputs = () => {
+		return jobFormFields.map((field, index) => {
+			let { hint, label, name } = field;
+			return <InputField index={index} hint={hint} label={label} name={name} />;
+		});
+	};
+
 	render() {
+		console.log(jobFormFields);
 		return (
 			<div className="job-form">
 				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-					<InputField
-						hint={"Ea. Full stack developer for x company"}
-						label={"Enter a Job Title"}
-						name={"title"}
-					/>
-					<InputField
-						hint={"Ea. Full stack developer opportunity"}
-						label={"Enter a Subject line for your job email"}
-						name={"subject"}
-					/>
-
-					<InputField
-						hint={"Ea. The job requires 3+ years of experience in..."}
-						label={"Enter the job description"}
-						name={"body"}
-					/>
-
-					<InputField
-						hint={"Ea. The job requires 3+ years of experience in..."}
-						label={"Enter your recipients"}
-						name={"reicipients"}
-					/>
+					{this.renderInputs()}
 					<RaisedButton type="submit" label="Submit" />
 				</form>
 			</div>
--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -16,3 +16,10 @@ export const dashboardRoutes =[
     img: '',
   }
 ]
+
+export const jobFormFields =[
+  {hint: "Ea. Full stack developer for x company", label: "Enter a Job Title", name: "title"},
+  {hint: "Ea. Full stack developer opportunity", label: "Enter a Subject line for your job email", name: "subject"},
+  {hint: "Ea. The job requires 3+ years of experience in...", label: "Enter the job description", name: "body"},
+  {hint: "Ea. The job requires 3+ years of experience in...", label: "Enter your recipients", name: "reicipients"},
+]
