605a44f9e3f5d1a17874227d1ec198a1dd6f00bc
Daniel Escobar <escobard90@gmail.com>
2018-05-14 18:00:51 -0600

    chore: fixed reducer test, it's important to note the Form reducers will attach itself to all reducers

--- client/test/reducers/reducers.test.js
+++ client/test/reducers/reducers.test.js
@@ -4,20 +4,22 @@ import toJson from "enzyme-to-json";
 
 describe("Reducers", () => {
 	it("renders the authReducer, with no state", () => {
-		expect(authReducer(undefined, {})).toEqual({ auth: null });
+		expect(authReducer(undefined, {})).toEqual({ auth: null, form: {} });
 	});
 	it("renders the authReducer, with un authenticated response", () => {
 		expect(
 			authReducer({ auth: "" }, { type: FETCH_USER, payload: "" })
 		).toEqual({
-			auth: false
+			auth: false,
+			form: {}
 		});
 	});
 	it("renders the authReducer, with authenticated response", () => {
 		expect(
 			authReducer({ auth: {} }, { type: FETCH_USER, payload: {} })
 		).toEqual({
-			auth: {}
+			auth: {},
+			form: {}
 		});
 	});
 	it("matches authReducer snapshot", () => {
